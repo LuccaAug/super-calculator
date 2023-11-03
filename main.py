db3353df131c2de4aac2bce2d05e8a3ad4463a4f
Lucca Augusto <lucca.aug@gmail.com>
2023-11-03 20:08:04 -0300

    feat: implements main function

--- main.py
+++ main.py
@@ -4,70 +4,52 @@ from computation.computation_calculator import ComputationCalculator
 from physic.physic_calculator import PhysicCalculator
 from trigonometric.trigonometric_calculator import TrigonometricCalculator
 
+options: dict = {
+    "C": "Computation",
+    "M": "Medicine",
+    "P": "Physic",
+    "T": "Trigonometric",
+    "E": "Exit",
+}
 
-class MainMenu:
-    options: dict = {
-        "C": "Computation",
-        "M": "Medicine",
-        "P": "Physic",
-        "T": "Trigonometric",
-        "E": "Exit",
-    }
-    calculator_area: BaseArea | None = None
+
+def main():
     in_loop: bool = True
+    while in_loop:
+        calculator_area: BaseArea | None = None
 
-    def __init__(self) -> None:
-        self.menu_in_loop()
-        self.end()
-
-    def reset_variables(self) -> None:
-        self.calculator_area = None
-
-    def show_menu(self) -> None:
         print("Choose the area: ")
-        for letter, name in self.options:
+        for letter, name in options:
             print("\t %s - %s" % (letter, name))
 
-    def menu_in_loop(self) -> None:
-        while self.in_loop:
-            self.reset_variables()
-            self.show_menu()
+        calculator_type: str = input()
+        calculator_type = calculator_type.upper()
 
-            calculator_type: str = input()
-            calculator_type = calculator_type.upper()
-
-            self.define_area(calculator_type)
-
-            if self.calculator_area is None:
-                print("Nothing was found, please try again!")
-                continue
-
-            self.instantiate_calculator_area()
-
-    def instantiate_calculator_area(self) -> None:
-        self.calculator_area.menu()
-        self.calculator_area.instantiate_calculator()
-
-    def define_area(self, calculator_type: str):
         if calculator_type == 'E':
-            self.in_loop = False
+            in_loop = False
             return
 
         elif calculator_type == 'M':
-            self.calculator_area = MedicineCalculator()
+            calculator_area = MedicineCalculator()
 
         elif calculator_type == 'C':
-            self.calculator_area = ComputationCalculator()
+            calculator_area = ComputationCalculator()
 
         elif calculator_type == 'P':
-            self.calculator_area = PhysicCalculator()
+            calculator_area = PhysicCalculator()
 
         elif calculator_type == 'T':
-            self.calculator_area = TrigonometricCalculator()
+            calculator_area = TrigonometricCalculator()
 
-    def end(self):
-        print("Thanks for using the super-calculator! :D")
+        if calculator_area is None:
+            print("Nothing was found, please try again!")
+            continue
+
+        calculator_area.menu()
+        calculator_area.instantiate_calculator()
+
+    print("Thanks for using the super-calculator! :D")
 
 
 if __name__ == "__main__":
-    MainMenu()
+    main()
