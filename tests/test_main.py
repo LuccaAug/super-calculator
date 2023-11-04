d4b349349dff1564648ce2a04ec82ada5d1084ee
Lucca Augusto <lucca.aug@gmail.com>
2023-11-03 21:22:11 -0300

    feat: implements tests to the main function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,29 @@
 import unittest
 
+from medicine.medicine_calculator import MedicineCalculator
+from main import MainMenu
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    menu: MainMenu
+
+    def setUp(self) -> None:
+        self.menu = MainMenu()
+
+    def test_number_of_options(self):
+        options = self.menu.options.keys()
+        len_options = len(options)
+
+        self.assertEqual(len_options, 5)
+
+    def test_exit(self):
+        self.menu.define_area('E')
+        self.assertFalse(self.menu.in_loop)
+
+    def test_area(self):
+        self.menu.define_area('M')
+        self.assertIsInstance(self.menu.calculator_area, MedicineCalculator)
+
 
 if __name__ == '__main__':
     unittest.main()
